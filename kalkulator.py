a494cf16f0b887a80f7c7f6f4a289d7a049a87f8
Lalu27 <156868563+Lalu27@users.noreply.github.com>
2024-01-17 11:06:19 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -11,8 +11,8 @@ pilihan = input("Masukkan Pilihan (1/2/3) :")
 if pilihan in ('1','2','3'):
   angka1 = int(input("Masukkan bilangan pertama :"))
   angka2 = int(input("Masukkan bilangan kedua :"))
-
-if pilihan == 1:
-  print(angka1,"+",angka2,"=", tambah(angka1, angka2))
-else:
-  print("Pilihan tidak valid")
+  
+  if pilihan == 1:
+    print(angka1,"+",angka2,"=", tambah(angka1, angka2))
+  else:
+    print("Pilihan tidak valid")
