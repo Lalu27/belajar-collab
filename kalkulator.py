01b9e0242822e4eaa79ea1fed423063ea6510c49
farismahdyakb <156868538+farismahdyakb@users.noreply.github.com>
2024-01-17 11:10:42 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,8 @@
 def tambah(a,b):
   return a+b
-
+def pengurangan(a,b):
+  return a-b
+  
 print("Pilih operasi :")
 print("1. Penjumlahan")
 print("2. Pengurangan")
@@ -11,10 +13,13 @@ pilihan = input("Masukkan Pilihan (1/2/3) :")
 if pilihan in ('1','2','3'):
   angka1 = int(input("Masukkan bilangan pertama :"))
   angka2 = int(input("Masukkan bilangan kedua :"))
-  
+
   if pilihan == 1:
     print(angka1,"+",angka2,"=", tambah(angka1, angka2))
-  elif pilihan == 3:
-    print(angka1,"*",angka2,"=", tambah(angka1, angka2))
+
+  elif pilihan == 2 :
+    print(angka1,"-",angka2,"=", pengurangan(angka1, angka2))
+  
   else:
-    print("Pilihan tidak valid")
+    print(pilihan)
+ 
