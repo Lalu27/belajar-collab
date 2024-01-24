6811f26cb1b524038fa2a4ad8582ce336557ee51
Lalu27 <firmansyahmuhasan@gmail.com>
2024-01-24 11:36:59 +0700

    menambahkan sisa bagi

--- kalkulator.py
+++ kalkulator.py
@@ -10,6 +10,7 @@ print("1. Penjumlahan")
 print("2. Pengurangan")
 print("3. Perkalian")
 print("4. Pembagian")
+print("5. Sisa bagi")
 pilihan = input("Masukkan Pilihan (1/2/3) :")
 
 if pilihan in ('1','2','3'):
